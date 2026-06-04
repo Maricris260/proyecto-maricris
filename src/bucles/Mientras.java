32c6adb7d5ec3756dbdebd6deb36192863c84396
maricris peje leon <maricrispejeleon>
2026-06-04 16:48:49 -0500

    clase de ejercicio bucles

---
+++ src/bucles/Mientras.java
@@ -0,0 +1,23 @@
+package bucles;
+
+public class Mientras {
+    public static void main(String[] args) {
+
+       /* while (condicion){//entonces
+            //bloque de codigo o instrucciones
+            //a ejecutar si es verdadero
+
+        }
+        /
+        */
+
+
+        int numero = 0;
+        while (numero <= 10){
+            System.out.println(numero);
+            //numero= numero +2;//forma extensa
+
+            numero+=2;//forma corta, usando operador de asignacion
+        }
+    }
+}
---
+++ src/bucles/Tablade5.java
@@ -0,0 +1,18 @@
+package bucles;
+
+public class Tablade5 {
+    public static void main(String[] args) {
+        //se permite crear un algoritmo
+        // que permita realisar la tabla del 5 hasta 10
+
+        int num = 1;
+        while (num <= 10){
+            System.out.println("5 x" + num + "=" + (5 * num));
+            num++;
+
+
+        }
+
+
+    }
+}
