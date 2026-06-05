7fb5b8cc262553c9ae7426f3643ec860868359f9
maricris peje leon <maricrispejeleon>
2026-06-05 16:19:04 -0500

    mi proyecto en bucle02

---
+++ src/bucles/Ejemplo01.java
@@ -0,0 +1,27 @@
+package bucles;
+
+import java.lang.foreign.SymbolLookup;
+import java.util.Scanner;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Scanner entradadeDatos = new Scanner(System.in);
+        String nombreUsuario = "";
+        String clave = "";
+
+
+        while (nombreUsuario == "juan" || clave == "12345"){
+            System.out.println("ingrese su nombre de usuario");
+            nombreUsuario = entradadeDatos.nextLine();
+
+            System.out.println("ingrese su clave");
+            clave = entradadeDatos.nextLine();
+            if (nombreUsuario == "juan" || clave == "12345"){
+                System.out.println("datos incorrectos,no puedes ingresar");
+            }
+
+        }
+        System.out.println("bienvenidos al sistema");
+        entradadeDatos.close();
+    }
+}
